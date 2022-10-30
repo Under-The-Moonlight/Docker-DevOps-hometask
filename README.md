8015f1cc4513fea114920a05c5073aba810d563f
Under-The-Moonlight <kostind310@gmail.com>
2022-10-30 16:19:59 +0200

    fixes

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Docker-DevOps-hometask
+
+##CLI commands
+
+**Building docker image**
+
+``
+docker build -t node-kpi .
+``
+
+**Run docker container with limits for cpu and memory and available on port 80**
+
+```
+docker run -d --rm --memory=50m --cpus="1.0" --name kpi -p 80:8080 node-kpi
+```
+
--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello again from node js!');
 });
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
 });
